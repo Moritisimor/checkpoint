2d1c652270539d5da05fabfd9730e399a2ed526d
Moritisimor <devMoritisimor@proton.me>
2025-11-26 06:54:23 +0100

    Added simple request routing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod models;
 use crate::models::{Config, GenericResponse};
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use awc::{body::BoxBody, http::Method};
 
 #[actix_web::get("/status")]
 async fn ping() -> impl Responder {
@@ -12,33 +13,68 @@ async fn ping() -> impl Responder {
 async fn route(config: web::Data<Config>, req: HttpRequest) -> impl Responder {
     let ip = match req.peer_addr() {
         Some(s) => s.ip().to_string(),
-        None => return HttpResponse::BadRequest(),
+        None => return HttpResponse::BadRequest().finish(),
     };
 
     if config.blacklist.contains(&ip) {
-        return HttpResponse::Forbidden();
+        return HttpResponse::Forbidden().finish();
     }
 
     let uri_string = req.uri().to_string();
     let params: Vec<&str> = uri_string.split("/").collect();
     let service = match params.get(1) {
         Some(s) => s,
-        None => return HttpResponse::BadRequest(),
+        None => return HttpResponse::BadRequest().finish(),
     };
 
-    let mut service_url = String::new();
+    let mut url = String::new();
     for s in &config.services {
         if s.mapping == *service {
-            service_url = s.url.clone();
+            url = s.url.clone();
             break;
         }
     }
 
-    if service_url.is_empty() {
-        HttpResponse::NotFound()
-    } else {
-        HttpResponse::Ok()
+    if url.is_empty() {
+        return HttpResponse::NotFound().finish()
     }
+
+    for p in &params[2..] {
+        url += "/";
+        url += p
+    }
+
+    println!("{url}");
+    let client = awc::Client::new();
+    let raw_response = match req.method() {
+        &Method::GET => client.get(url).send().await,
+        &Method::POST => client.post(url).send().await,
+        &Method::DELETE => client.delete(url).send().await,
+        &Method::HEAD => client.head(url).send().await,
+        &Method::OPTIONS => client.options(url).send().await,
+        &Method::PATCH => client.patch(url).send().await,
+        &Method::PUT => client.put(url).send().await,
+        _ => return HttpResponse::MethodNotAllowed().finish()
+    };
+    
+    let mut res = match raw_response {
+        Ok(res) => res,
+        Err(_) => return HttpResponse::BadGateway().finish()
+    };
+
+    let body = match res.body().await {
+        Ok(b) => b,
+        Err(_) => return HttpResponse::InternalServerError().finish()
+    };
+
+    let mut forwarded = HttpResponse::new(*&res.status())
+        .set_body(BoxBody::new(body));
+
+    for (k, v) in res.headers() {
+        forwarded.headers_mut().append(k.clone(), v.clone());
+    }
+
+    forwarded
 }
 
 #[actix_web::main]
